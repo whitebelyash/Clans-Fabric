b4f7e41c6f9760e098b3f90d09b4fd222ac75fb2
whbex <66699673+whitebelyash@users.noreply.github.com>
2024-09-08 12:49:29 +0400

    Rework init logic: separated init/enable/shutdown
    Required for MinecraftServer instance

--- src/main/java/ru/whbex/develop/clans/fabric/MainFabric.java
+++ src/main/java/ru/whbex/develop/clans/fabric/MainFabric.java
@@ -5,9 +5,12 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.MinecraftVersion;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
@@ -32,6 +35,8 @@ import ru.whbex.develop.clans.fabric.wrap.PlayerActorFabric;
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
 import java.util.*;
 import java.util.concurrent.Callable;
@@ -46,9 +51,16 @@ public class MainFabric implements DedicatedServerModInitializer, ClansPlugin {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	private ConsoleActor consoleActor;
 	private ConfigWrapper configWrapper;
+	private File workdir;
+
+	private ClanManager clanManager;
 	private SQLAdapter adapter;
+
+	private MinecraftServer server;
 	private Language lang;
 	private final Map<UUID, PlayerActor> actors = new HashMap<>();
+	private final Map<String, PlayerActor> actorsByName = new HashMap<>();
+
 
 	private ExecutorService dbExecutor;
 
@@ -56,17 +68,35 @@ public class MainFabric implements DedicatedServerModInitializer, ClansPlugin {
 
 	@Override
 	public void onInitializeServer() {
+
+		/* Context init */
 		Context.INSTANCE.logger = LOGGER;
 		Context.INSTANCE.plugin = this;
 		this.consoleActor = new ConsoleActorFabric();
+
+		/* Server lifecycle callbacks */
+		ServerLifecycleEvents.SERVER_STARTING.register((server1 -> {
+			this.server = server1;
+            try {
+                this.onPostInitialize();
+            } catch (IOException | ClassNotFoundException | SQLException e) {
+				ClansPlugin.log(Level.INFO, "Plugin init failed !!!");
+                throw new RuntimeException(e);
+            }
+        }));
+		ServerLifecycleEvents.SERVER_STOPPING.register(server1 -> this.onShutdown());
+		ClansPlugin.log(Level.INFO, "Registered server lifecycle callbacks, continuing init");
+
+		/* Startup */
 		ClansPlugin.dbg("hello");
 		ClansPlugin.log(Level.INFO, "=== Clans ===");
 		ClansPlugin.log(Level.INFO, "Running on Fabric (Minecraft " + MinecraftVersion.CURRENT.getName() + ")");
 		ClansPlugin.dbg("Config dir: " + FabricLoader.getInstance().getConfigDir().toString());
-		File workdir = FabricLoader.getInstance().getConfigDir().toFile();
+		workdir = FabricLoader.getInstance().getConfigDir().toFile();
 
-
+		/* Config init */
 		this.configWrapper = new ConfigWrapperFabric();
+		File f = new File(workdir, "messages.lang"); // TODO: new name
 		// TODO: Use assets for this
 	//	LangFile lf = new LangFile(new File(workdir, "messages.lang"));
 	//	lang = new Language(lf);
@@ -75,40 +105,49 @@ public class MainFabric implements DedicatedServerModInitializer, ClansPlugin {
 		ClansPlugin.log(Level.INFO, "Starting database executor thread");
 		dbExecutor = Executors.newSingleThreadExecutor();
 
-		// TODO: change exception handling
-        try {
-            adapter = new H2SQLAdapter(new File(workdir, MOD_ID + "_database.h2"));
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-		ClansPlugin.log(Level.INFO, "Connecting to db");
-		// TODO: same
-        try {
-            adapter.connect();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
+		ClansPlugin.log(Level.INFO, "=== Early init completed! ===");
+    }
+
+	private void onPostInitialize() throws IOException, ClassNotFoundException, SQLException {
+		ClansPlugin.log(Level.INFO, "=== Starting up ===");
+
+		adapter = new H2SQLAdapter(new File(workdir, MOD_ID + "_database.h2"));
+		adapter.connect();
+
+		// initial db update
+		// TOOD: move to ClansPlugin
+		adapter.update("CREATE TABLE IF NOT EXISTS clans (id varchar(36), tag varchar(16), " +
+					"name varchar(24), " +
+					"description varchar(255), " +
+					"creationEpoch LONG, " + // TODO: fixxx
+					"leader varchar(36), " +
+					"deleted TINYINT, " +
+					"level INT, " +
+					"exp INT);");
+
 		Bridge b = adapter == null ? new NullBridge() : new SQLBridge(adapter);
-		ClanManager clanManager = new ClanManager(configWrapper, b);
+		clanManager = new ClanManager(configWrapper, b);
 
 		ClansPlugin.log(Level.INFO, "Registering event callbacks");
 		ServerPlayConnectionEvents.JOIN.register(((handler, sender, server) -> {
-			ClansPlugin.dbg("register actor " + handler.getPlayer().getUuid());
-			PlayerActor actor = actors.containsKey(handler.getPlayer().getUuid()) ?
-					((PlayerActorFabric) actors.get(handler.getPlayer().getUuid())).updateEntity(handler.getPlayer()) :
-					new PlayerActorFabric(handler.getPlayer());
-			actors.put(actor.getUniqueId(), actor);
 			ClansPlugin.dbg("ok");
 
 		}));
 		ServerPlayConnectionEvents.DISCONNECT.register(((handler, server) -> {
-            actors.remove(handler.getPlayer().getUuid());
+			actors.remove(handler.getPlayer().getUuid());
 		}));
 
-		ClansPlugin.log(Level.INFO, "Startup completed");
-    }
+		ClansPlugin.log(Level.INFO, "=== Startup completed! ===");
+
+	}
+	private void onShutdown(){
+		ClansPlugin.log(Level.INFO, "=== Shutting down ===");
+		// shutdown tasks going here
+
+		ClansPlugin.log(Level.INFO, "Complete, goodbye!");
+
+	}
 
 	@Override
 	public ConsoleActor getConsoleActor() {
@@ -117,12 +156,12 @@ public class MainFabric implements DedicatedServerModInitializer, ClansPlugin {
 
 	@Override
 	public PlayerActor getPlayerActor(UUID uuid) {
-		return null;
+		return actors.get(uuid);
 	}
 
 	@Override
 	public PlayerActor getPlayerActor(String s) {
-		return null;
+		return actorsByName.get(s);
 	}
 
 	@Override
@@ -132,12 +171,12 @@ public class MainFabric implements DedicatedServerModInitializer, ClansPlugin {
 
 	@Override
 	public Collection<PlayerActor> getOnlineActors() {
-		return List.of();
+		return actors.values();
 	}
 
 	@Override
 	public ClanManager getClanManager() {
-		return null;
+		return clanManager;
 	}
 
 	@Override
